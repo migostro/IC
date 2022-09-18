b6183120180e2ecd214bb2e7d397889f1477f771
migostro <migostro@hotmail.com>
2022-09-18 20:55:07 -0300

    adicionado build para a compilação do cmake && make do build

---
+++ build/CMakeFiles/node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/node.dir/node.cpp.o"
+  "CMakeFiles/node.dir/node.cpp.o.d"
+  "node.cpython-38-x86_64-linux-gnu.so"
+  "node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
