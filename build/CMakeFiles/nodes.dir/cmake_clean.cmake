36f649a3131a6b1bb96505affce1c81b980dc1bb
migostro <migostro@hotmail.com>
2022-09-19 12:12:53 -0300

    atualiza o build (deixa ele vazio)

---
+++ build/CMakeFiles/nodes.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nodes.dir/node.cpp.o"
+  "CMakeFiles/nodes.dir/node.cpp.o.d"
+  "nodes.cpython-38-x86_64-linux-gnu.so"
+  "nodes.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nodes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
