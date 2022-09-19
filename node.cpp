2e8b8fe328c4767fe2ced2d669a0364d65df20e6
migostro <migostro@hotmail.com>
2022-09-19 11:18:40 -0300

    funções do nodes adicionados para o wrapper

--- node.cpp
+++ node.cpp
@@ -1,6 +1,16 @@
+// links relevantes para o pybind11
+// https://www.youtube.com/watch?v=_5T70cAXDJ0
+// https://pybind11.readthedocs.io/en/stable/basics.html
+// https://gist.github.com/safijari/f7aec85b89906b4b90a8f33039c11263
+
+
+// TODO: adicionar no 
+// git clone https://github.com/pybind/pybind11.git
+
+
 #include "node.h"
 
-#include <vector>
+namespace py = pybind11;
 
 using namespace std;
 
@@ -12,222 +22,143 @@ Node::Node(){
 
 /* adiciona fluxo que passa pelo vertice 
 */
-void Node::add_flow(long long int amount){
+void Node::add_flow(lli amount){
     flow_amount += amount;
 }
 
-void Node::add_adj(long long int v){
+void Node::add_adj(lli v){
     adj.push_back({0, v});
 }
 
-void Node::add_father(long long int v){
+void Node::add_father(lli v){
     fathers.push_back(v);
 }
 
 /* Recebe a posição do vetor adj que será inserido o peso (E NÃO VERTICE) e o peso
 */
-void Node::set_weight(long long int i, long long int weight){
+void Node::set_weight(lli i, lli weight){
     get<0>(adj[i]) = weight;
 }
 
-vector<pair<long long int, long long int>> Node::get_adj(){
+std::vector<std::pair<lli, lli>> Node::get_adj(){
     return adj;
 }
 
-pair<long long int, long long int> Node::get_adj(long long int i){
+std::pair<lli, lli> Node::get_adj(lli i){
     return adj[i];
 }
 
-long long int Node::fathers_size(){
+lli Node::fathers_size(){
     return fathers.size();
 }
 
-long long int Node::get_weight(long long int i){
-    return get<0> (adj[i]);
+lli Node::get_weight(lli i){
+    return get<0>(adj[i]);
 }
 
-long long int Node::get_flow_amount(){
+lli Node::get_flow_amount(){
     return flow_amount;
 }
 
-long long int Node::num_fathers(){
+lli Node::num_fathers(){
     return fathers.size();
 }
 
-long long int Node::adj_size(){
+lli Node::adj_size(){
     return adj.size();
 }
 
 
+/*************************************** NODES ****************************************/
+Nodes::Nodes(std::vector<lli>& out, std::vector<lli>& in, lli n){
+    std::vector<Node> novo(n);
+    node_vector = novo;
+    for (lli i = 0; i < n-1; i++){
+        node_vector[out[i]].add_adj(in[i]);
+        node_vector[in[i]].add_father(out[i]);
+    }
+}
 
+void Nodes::add_flow(lli v, lli amount){
+    node_vector[v].add_flow(amount);
+}
 
+void Nodes::add_adj(lli v, lli u){
+    node_vector[v].add_adj(u);
+    node_vector[u].add_father(v);
+}
 
+/* Recebe um vertice, uma posição do vetor adj (E NÃO VERTICE) que será inserido o peso e o peso
+*/
+void Nodes::set_weight(lli v, lli i, lli weight){
+    node_vector[v].set_weight(i, weight);
+}
 
+std::vector<std::pair<lli, lli>> Nodes::adj(lli v){
+    return node_vector[v].get_adj();
+}
 
+lli Nodes::num_fathers(lli v){
+    return node_vector[v].fathers_size();
+}
 
+lli Nodes::weight(lli i, lli j){
+    return node_vector[i].get_weight(j);
+}
 
+lli Nodes::get_flow_amount(lli i){
+    return node_vector[i].get_flow_amount();
+}
 
-
-
-
-
-
-
-
-/* NODES */
-// Nodes::Nodes(long long int n){
-//     vector<Node> novo(n);
-//     node_vector = novo;
-// }
-
-// void Nodes::add_flow(long long int v, long long int amount){
-//     node_vector[v].add_flow(amount);
-// }
-
-// void Nodes::add_adj(long long int v, long long int u){
-//     node_vector[v].add_adj(u);
-//     node_vector[u].add_father(v);
-// }
-
-// /* Recebe um vertice, uma posição do vetor adj (E NÃO VERTICE) que será inserido o peso e o peso
-// */
-// void Nodes::set_weight(long long int v, long long int i, long long int weight){
-//     node_vector[v].set_weight(i, weight);
-// }
-
-// vector<pair<long long int, long long int>> Nodes::adj(long long int v){
-//     return node_vector[v].get_adj();
-// }
-
-// long long int Nodes::num_fathers(long long int v){
-//     return node_vector[v].fathers_size();
-// }
-
-// long long int Nodes::weight(long long int i, long long int j){
-//     return node_vector[i].get_weight(j);
-// }
-
-// long long int Nodes::get_flow_amount(long long int i){
-//     return node_vector[i].get_flow_amount();
-// }
-
-
-
-/*
-
-
-#include <iostream>
-#include <vector>
-#include <queue>
-#include "node.h"
-
-using namespace std;
-
-void calcula_fluxo(vector<Node> nodes, long long int n){
-    vector<long long int> accessed(n);
-    vector<long long int> leaves;
-    queue<long long int> queue;
-    long long int current, son, flow;
+void Nodes::calcula_fluxo(){
+    lli n = node_vector.size();
+    std::vector<lli> accessed(n);
+    std::queue<lli> queue;
+    lli current, son, flow;
 
     // encontra e guarda as folhas do grafo
-    for (long long int i = 0; i < n; i++)
+    for (lli i = 0; i < n; i++)
     {
-        if (nodes[i].num_fathers() == 0)
+        if (node_vector[i].num_fathers() == 0)
         {
-            cout << "flux 1" << endl;
-            cout << i << endl;
-            cout << nodes.size() << endl;
-            cout << "num adj: " << nodes[i].get_adj().size();
-            nodes[i].set_weight(0, 1);
-            leaves.push_back(i);
-            cout << "flux 2" << endl;
+            node_vector[i].set_weight(0, 1);
+            queue.push(i);
         }
-    }
-
-    for (long long int leaf: leaves)
-    {
-        queue.push(leaf);
     }
 
     while (!queue.empty())
     {
-        cout << "flux 3" << endl;
         current = queue.front();
         queue.pop();
 
-
-        while (accessed[current] == nodes[current].num_fathers() && nodes[current].get_adj().size() > 0)
+        while (accessed[current] == node_vector[current].num_fathers() && node_vector[current].adj_size() > 0)
         {
-            cout << "flux 4" << endl;
-            son = get<1>(nodes[current].get_adj()[0]);
+            son = get<1>(node_vector[current].get_adj()[0]);
 
-            flow = nodes[current].get_flow_amount();
+            flow = node_vector[current].get_flow_amount();
 
             // adiciona fluxo na aresta que vai de current para son
-            nodes[current].set_weight(0, flow);
+            node_vector[current].set_weight(0, flow);
 
             // adiciona fluxo no vertice son
-            nodes[son].add_flow(flow);
+            node_vector[son].add_flow(flow);
 
             current = son;
             accessed[current]++;
-            cout << "flux 5" << endl;
         }
-        cout << "flux 6" << endl;
     }
-    // for (long long int i = 0; i < n; i++)
-    // {
-    //     cout << i << ": " << accessed[i] << endl;
-    // }
 }
 
-// long long int grau(){
-
-// }
-
-// double assortatividade(Nodes* nodes, long long int n){
-//     // quantidade de vertices com cada grau
-//     vector<long long int> num_grau(n-1, 0);
-
-//     for (long long int i = 0; i < n; i++)
-//     {
-        
-//     }
-    
-// }
-
-int main(){
-    
-    long long int n, a, b;
-    cin >> n;
-
-    vector<Node> nodes(n);
-
-    for (long long int i = 0; i < n-1; i++)
-    {
-        cin >> a >> b;
-        nodes[a].add_adj(b);
-    }
-
-    cout << "passou" << endl;
-
-    calcula_fluxo(nodes, n);
-
-    cout << "Fluxo em cada vertice do grafo: " << endl;
-
-    // for (long long int i = 0; i < n; i++)
-    // {
-    //     if(nodes->adj(i).size() > 0)
-    //         cout << nodes->weight(i, 0) << endl;
-    // }
-
-    for (long long int i = 0; i < n; i++)
-    {
-        cout << i << ": " << nodes[i].get_flow_amount() << endl;
-    }
-    
-    return 0;
+// define as funções que podem ser usadas no python
+PYBIND11_MODULE(nodes, handle) {
+    py::class_<Nodes>(handle, "Nodes")
+        .def(py::init<std::vector<lli>&, std::vector<lli>&, lli>())
+        .def("add_flow", &Nodes::add_flow)
+        .def("add_adj", &Nodes::add_adj)
+        .def("set_weight", &Nodes::set_weight)
+        .def("adj", &Nodes::adj)
+        .def("num_fathers", &Nodes::num_fathers)
+        .def("weight", &Nodes::weight)
+        .def("get_flow_amount", &Nodes::get_flow_amount)
+        .def("calcula_fluxo", &Nodes::calcula_fluxo);
 }
-
-
-*/
--- node.h
+++ node.h
@@ -3,144 +3,49 @@
 
 //#include <vector>
 #include <bits/stdc++.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
-using namespace std;
+
+#define lli long long int
 
 class Node{
     private:
         // quantidade de fluxo que passa pelo vertice
-        long long int flow_amount;
+        lli flow_amount;
         // vetor de pair, onde o peso é o first e o adjacente é o second
-        vector<pair<long long int, long long int>> adj;
-        vector<long long int> fathers;
+        std::vector<std::pair<lli, lli>> adj;
+        std::vector<lli> fathers;
     public:
         Node();
-        void add_flow(long long int amount);
-        void add_adj(long long int v);
-        void add_father(long long int v);
-        void set_weight(long long int i, long long int weight);
-        vector<pair<long long int, long long int>> get_adj();
-        pair<long long int, long long int> get_adj(long long int i);
-        long long int fathers_size();
-        long long int get_weight(long long int i);
-        long long int get_flow_amount();
-        long long int num_fathers();
-        long long int adj_size();
+        void add_flow(lli amount);
+        void add_adj(lli v);
+        void add_father(lli v);
+        void set_weight(lli i, lli weight);
+        std::vector<std::pair<lli, lli>> get_adj();
+        std::pair<lli, lli> get_adj(lli i);
+        lli fathers_size();
+        lli get_weight(lli i);
+        lli get_flow_amount();
+        lli num_fathers();
+        lli adj_size();
 };
 
 
-// class Nodes{
-//     private:
-//         vector<Node> node_vector;
-//     public:
-//         Nodes(long long int n);
-//         void add_flow(long long int v, long long int amount);
-//         void add_adj(long long int v, long long int u);
-//         void set_weight(long long int v, long long int i, long long int weight);
-//         vector<pair<long long int, long long int>> adj(long long int v);
-//         long long int num_fathers(long long int v);
-//         long long int weight(long long int i, long long int j);
-//         long long int get_flow_amount(long long int i);
-// };
+class Nodes{
+    private:
+        std::vector<Node> node_vector;
+    public:
+        Nodes(std::vector<lli>& out, std::vector<lli>& in, lli n);
+        void add_flow(lli v, lli amount);
+        void add_adj(lli v, lli u);
+        void set_weight(lli v, lli i, lli weight);
+        std::vector<std::pair<lli, lli>> adj(lli v);
+        lli num_fathers(lli v);
+        lli weight(lli i, lli j);
+        lli get_flow_amount(lli i);
+        void calcula_fluxo();
+};
 
 
 #endif
-
-/*#include <iostream>
-#include <vector>
-#include <queue>
-#include "node.h"
-
-using namespace std;
-
-void calcula_fluxo(Nodes * nodes, long long int n){
-    vector<long long int> accessed(n);
-    vector<long long int> leaves;
-    queue<long long int> queue;
-    long long int current, son, flow;
-
-    // encontra e guarda as folhas do grafo
-    for (long long int i = 0; i < n; i++)
-    {
-        if (nodes->num_fathers(i) == 0)
-        {
-            nodes->set_weight(i, 0, 1);
-            leaves.push_back(i);
-        }
-    }
-
-    for (long long int leaf: leaves)
-    {
-        queue.push(leaf);
-    }
-
-    while (!queue.empty())
-    {
-        current = queue.front();
-        queue.pop();
-
-
-        while (accessed[current] == nodes->num_fathers(current) && nodes->adj(current).size() > 0)
-        {
-            son = get<1>(nodes->adj(current)[0]);
-
-            flow = nodes->get_flow_amount(current);
-
-            // adiciona fluxo na aresta que vai de current para son
-            nodes->set_weight(current, 0, flow);
-
-            // adiciona fluxo no vertice son
-            nodes->add_flow(son, flow);
-
-            current = son;
-            accessed[current]++;
-        }
-        
-    }
-    // for (long long int i = 0; i < n; i++)
-    // {
-    //     cout << i << ": " << accessed[i] << endl;
-    // }
-}
-
-// double assortatividade(Nodes* nodes, long long int n){
-//     // quantidade de vertices com cada grau
-//     vector<long long int> num_grau(n-1, 0);
-
-//     for (long long int i = 0; i < n; i++)
-//     {
-        
-//     }
-    
-// }
-
-int main(){
-    
-    long long int n, a, b;
-    cin >> n;
-
-    Nodes * nodes = new Nodes(n);
-
-    for (long long int i = 0; i < n-1; i++)
-    {
-        cin >> a >> b;
-        nodes->add_adj(a, b);
-    }
-
-    calcula_fluxo(nodes, n);
-
-    cout << "Fluxo em cada vertice do grafo: " << endl;
-
-    // for (long long int i = 0; i < n; i++)
-    // {
-    //     if(nodes->adj(i).size() > 0)
-    //         cout << nodes->weight(i, 0) << endl;
-    // }
-
-    for (long long int i = 0; i < n; i++)
-    {
-        cout << i << ": " << nodes->get_flow_amount(i) << endl;
-    }
-    
-    return 0;
-}*/
